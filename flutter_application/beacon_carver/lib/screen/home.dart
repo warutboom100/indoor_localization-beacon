c9ec9ce11f874e89009002f66e5d37c6f4ff3601
Iboomnarak <60059486+warutboom100@users.noreply.github.com>
2022-09-09 16:06:05 +0700

    demo2

--- flutter_application/beacon_carver/lib/screen/home.dart
+++ flutter_application/beacon_carver/lib/screen/home.dart
@@ -12,6 +12,58 @@ class HomeScreen extends StatelessWidget {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 colors: [Color(0xFF02021A), Color(0xFF072F71)])),
+        child: Scaffold(
+          backgroundColor: Colors.white.withOpacity(0.0),
+          body: SafeArea(
+              child: Column(
+            children: [
+              Container(
+                child: SizedBox(height: 40),
+              ),
+              Container(
+                  child: Column(children: [
+                Image.asset('assets/images/Logo.png'),
+              ])),
+              Container(
+                width: 320,
+                margin: EdgeInsets.fromLTRB(16, 150, 16, 8),
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return RegisterScreen();
+                        }));
+                      },
+                      icon: Icon(Icons.add),
+                      label: Text(
+                        "Register",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                ),
+              ),
+              Container(
+                width: 320,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return LoginScreen();
+                        }));
+                      },
+                      icon: Icon(Icons.login),
+                      label: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 20),
+                      )),
+                ),
+              )
+            ],
+          )),
+        ),
       ),
     );
   }
